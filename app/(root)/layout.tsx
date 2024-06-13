69c838d8437ad19587728ca735a353dec57c495f
Kubilay Türe <kubilayture@gmail.com>
2024-06-14 00:28:05 +0300

    static assets and leftSidebar component

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,28 @@
+import LeftSidebar from '@/components/LeftSidebar';
+import Image from 'next/image';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className='relative flex flex-col'>
+      <main className='relative flex bg-black-3'>
+        <LeftSidebar />
+        <section>
+          <div>
+            <div>
+              <Image />
+              Mobile Nav
+            </div>
+          </div>
+          Toaster
+          {children}
+        </section>
+
+        <p className='text-white-1'>RIGHT SIDEBAR</p>
+      </main>
+    </div>
+  );
+}
