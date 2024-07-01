03dcd002aed94e9047c06d87799e5e178cf18167
Kubilay Türe <kubilayture@gmail.com>
2024-07-01 18:59:41 +0300

    listing podcasts

--- convex/podcasts.ts
+++ convex/podcasts.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { mutation } from './_generated/server';
+import { mutation, query } from './_generated/server';
 
 export const getUrl = mutation({
   args: {
@@ -58,4 +58,11 @@ export const createPodcast = mutation({
       audioDuration: args.audioDuration,
     });
   },
+});
+
+export const getTrendingPodcasts = query({
+  handler: async (ctx) => {
+    const podcast = await ctx.db.query('podcasts').collect();
+    return podcast;
+  }
 });
